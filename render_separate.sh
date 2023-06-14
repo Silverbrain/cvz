ed99a7a0cd170faadbd0b3a07f998ab2b07dca07
Sina Ataei <sinaataei@outlook.com>
2023-06-14 12:31:38 +0100

    Opening pdf files capabilities added

--- render_separate.sh
+++ render_separate.sh
@@ -3,6 +3,22 @@
 TEX_FILE_DIR="sections"
 OUTPUT_DIR="aux"
 
+OPEN_FILES=false
+
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        -O|--open-pdf)
+            OPEN_FILES=true
+            shift
+        ;;
+        *)
+            echo "Invalide option.\n"
+            echo "Usage: $0 [-O|--open-pdf]"
+            exit 1
+        ;;
+    esac
+done
+
 if [ -d "$OUTPUT_DIR" ]; then
     echo "The directory exists."
 else
@@ -19,3 +35,9 @@ for TEX_FILE in "$TEX_FILE_DIR"/*.tex; do
         return 0
     fi
 done
+
+if $OPEN_FILES; then
+    for PDF_FILE in *.pdf; do
+        open $PDF_FILE
+    done
+fi
